20e17d256a62ed6047e0fcef989a039d53a0a251
Alina <alina.altynbaeva.00@mail.ru>
2021-08-08 17:54:43 +0200

    commit instance

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -8,7 +8,7 @@ export default function Sidebar() {
   const [cats, setCats] = useState([])
 
   const getCats = async () => {
-    const res = await axios.get('/categories')
+    const res = await axios.get('/categories/')
     setCats(res.data)
   }
 
