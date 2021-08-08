dbb07efc476263d0257acecd5039159b079cb771
Alina <alina.altynbaeva.00@mail.ru>
2021-08-08 17:31:55 +0200

    commit instance

--- src/baseUrl.js
+++ src/baseUrl.js
@@ -4,9 +4,7 @@ const instance = axios.create({
   baseURL: 'https://safe-dawn-28691.herokuapp.com/',
 })
 
-instace.defaults.headers.common(
-  'Access-Control-Allow-Origin',
+instance.defaults.headers.common['Access-Control-Allow-Origin'] =
   'http://localhost:3000'
-)
 
 export default instance
